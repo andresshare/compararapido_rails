d2cffa95aef8de3b93efefe7df9f192d28817255
andresshare <andresshare@gmail.com>
2017-09-20 18:19:34 -0500

    created route POST/products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,4 +5,17 @@ end
 def new
   @product = Product.new
 end
+def create
+  @product= Product.new(product_params)
+  if @product.save
+    redirect_to products_path, notice:"You publish OK"
+  else
+    render :new
+  end
+end
+
+private
+  def product_params
+   params.require(:product).permit(:name, :url, :description)
+ end
 end

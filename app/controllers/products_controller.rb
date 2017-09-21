66eb1c002f8efba518242837cf5d9167013a53d8
andresshare <andresshare@gmail.com>
2017-09-20 22:37:15 -0500

    created route DELETE

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,12 +22,19 @@ end
 end
 
 def update
-  @product =Product.find(params[:id])
-  if @product.update(product_params)
+  product =Product.find(params[:id])
+  if product.update(product_params)
     redirect_to product_path, notice: "your product has been successfully modified"
   else
    render :edit
   end
+end
+
+def destroy
+  product =Product.find(params[:id])
+  product.destroy
+
+  redirect_to products_path,notice: "your product has been successfully modified"
 end
 
 private

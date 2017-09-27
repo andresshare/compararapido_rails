b32903877330a5b3442faf41f71291028e6a3873
andresshare <andresshare@gmail.com>
2017-09-26 22:52:43 -0500

    funcionalidad de autenticar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  root 'products#index'
+  
+  get 'login',to: 'sessions#new'
+  post 'login',to: 'sessions#create'
 
   resources :users,only:[:new, :create]
   resources  :products 
-  root 'products#index'
+  
 
 end

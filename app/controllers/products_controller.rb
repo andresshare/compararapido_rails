05ccc21b5de9fc346f1b1e1be7b14d3d325fa82d
andresshare <andresshare@gmail.com>
2017-09-29 17:46:23 -0500

    asocio usuario to productos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,49 +1,50 @@
 class ProductsController < ApplicationController
+  before_action :private_access, except: [:index, :show]
 
-before_action :private_access, except: [:index, :show]  
-
-
-def index
-  @products = Product.all
-end
-def new
-  @product = Product.new
-end
-def create
-  @product= Product.new(product_params)
-  if @product.save
-    redirect_to products_path, notice:"You publish OK"
-  else
-    render :new
+  def index
+    @products = Product.all
   end
-end
- def show
-   @product =Product.find(params[:id])
- end
 
- def edit
-  @product =Product.find(params[:id])
-end
-
-def update
-  product =Product.find(params[:id])
-  if product.update(product_params)
-    redirect_to product_path, notice: "your product has been successfully modified"
-  else
-   render :edit
+  def new
+    @product = Product.new
   end
-end
 
-def destroy
-  product =Product.find(params[:id])
-  product.destroy
+  def create
+    @product = Product.new(product_params)
+    @product.user = current_user
+    if @product.save
+      redirect_to products_path, notice: "El producto fue publicado con éxito"
+    else
+      render :new
+    end
+  end
 
-  redirect_to products_path,notice: "your product has been successfully modified"
-end
+  def show
+    @product = Product.find(params[:id])
+  end
 
-private
-  def product_params
-   params.require(:product).permit(:name, :url, :description)
- end
+  def edit
+    @product = Product.find(params[:id])
+  end
 
+  def update
+    product = Product.find(params[:id])
+    if product.update(product_params)
+      redirect_to products_path, notice: "El producto ha sido modificado con éxito"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+
+    redirect_to products_path, notice: "El producto fue eliminado con éxito"
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :url, :description, :image)
+    end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
 
+belongs_to :user
 validates :name,presence:true
 validates :url,presence:true
 end
